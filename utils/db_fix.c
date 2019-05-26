50694444e363c67290e5ee8de583928e6568b2b3
Tom Trebisky <tom@mmto.org>
2019-05-26 16:44:56 -0700

    More fiddling.

--- utils/db_fix.c
+++ utils/db_fix.c
@@ -15,6 +15,9 @@
 
 #include "oven.h"
 
+void fix_biparam ( b_database * );
+void fix_param ( p_database * );
+
 struct bp_database {
 	b_database      biparameter;
         p_database      parameter;
@@ -23,30 +26,52 @@ struct bp_database {
 struct bp_database buf;
 struct stat sbuf;
 
+#ifdef notdef
+/* POSIX has this these days */
 void
-fixer ( char *file )
+my_rename ( char *cur_name, char *bak_name )
+{
+	/* If there is anything already there, away it goes ! */
+	unlink ( bak_name );
+
+	/* two names briefly */
+        link ( cur_name, bak_name);
+
+	/* abandon the original name */
+	unlink ( cur_name );
+}
+#endif
+
+void
+fixer ( char *file, char *bak_file )
 {
 	int nw = sizeof ( buf );
 	int nr;
-	int ifd;
+	int fd;
 
 	// printf ( "Size of BP is %d bytes\n", nw );
 	// Size of BP is 133548 bytes (correct)
 
-	ifd = open ( file, O_RDONLY );
+	fd = open ( file, O_RDONLY );
+	if ( fd < 0 ) {
+	    fprintf ( stderr, "Cannot open input file\n" );
+	    exit ( 1 );
+	}
 
-	(void) fstat ( ifd, &sbuf );
+	(void) fstat ( fd, &sbuf );
 	if ( sbuf.st_size != nw ) {
 	    fprintf ( stderr, "Wrong file size: %d bytes\n", sbuf.st_size );
 	    exit ( 1 );
 	}
 
-	nr = read ( ifd, (void *) &buf, nw );
+	nr = read ( fd, (void *) &buf, nw );
 	if ( nr != nw ) {
 	    fprintf ( stderr, "Bad file read: %d bytes\n", nr );
 	    exit ( 1 );
 	}
-	(void) close ( ifd );
+	(void) close ( fd );
+
+	rename ( file, bak_file );
 
 #ifdef notdef
 	/* It would be nice to recognize what endianism the file
@@ -56,15 +81,32 @@ fixer ( char *file )
 	printf ( "Z0 clock: %08x\n", buf.biparameter.zone[0].clock );
 	printf ( "Timestamp: %08x\n", buf.parameter.misc.timestamp );
 #endif
+	printf ( "Z0 clock: %08x\n", buf.biparameter.zone[0].clock );
+	printf ( "Timestamp: %08x\n", buf.parameter.misc.timestamp );
+
+	fix_param ( &buf.parameter );
+	fix_biparam ( &buf.biparameter );
+
+	fd = open ( file, O_WRONLY|O_CREAT, 0644 );
+	if ( fd < 0 ) {
+	    fprintf ( stderr, "Cannot create output file\n" );
+	    exit ( 1 );
+	}
+	(void) write ( fd, (void *) &buf, nw );
+	(void) close ( fd );
 
 	exit ( 0 );
 
 }
 
+/* This needs command line arguments.
+ * If there is just one argument, discard the original file.
+ * If there are two, the second is the backup name.
+ */
 int
 main ( int argc, char **argv )
 {
-	fixer ( "database" );
+	fixer ( "database", "database.OLD" );
 }
 
 /* THE END */
